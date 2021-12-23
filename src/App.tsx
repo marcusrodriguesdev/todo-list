552d8fd1b719535f2c20a15a39e4987876ce9afc
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-23 16:13:18 -0300

    Finish Add area

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,23 @@ import { ListItem } from './components/ListItem';
 import { Item } from './types/Item';
 
 const App = () => {
-  const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Viajar com familia', done: false},
-    { id: 2, name: 'Estudar Typescript', done: false}
-  ]);
+  const [list, setList] = useState<Item[]>([]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  }
 
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
-        <AddArea />
+        <AddArea onEnter={handleAddTask} />
         { list.map((item, index) => (
           <ListItem
             key={index}
--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,13 +1,29 @@
+import { useState, KeyboardEvent } from 'react';
 import * as C from './styles';
 
-export const AddArea = () => {
+type HandleText = {
+  onEnter: (taskName: string) => void
+};
+
+export const AddArea = ({ onEnter }: HandleText) => {
+  const [inputText, setInputText] = useState(''); 
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.code === 'Enter' && inputText !== '') {
+       onEnter(inputText);
+       setInputText('');
+    }
+  }
+
   return (
     <C.Container>
       <div className="image">➕</div>
       <input
+        value={inputText}
         type="text"
-        name=""
         placeholder="Adicione uma tarefa"
+        onChange={ ({ target }) => setInputText(target.value) }
+        onKeyUp={handleKeyUp}
       />
     </C.Container>
   );
