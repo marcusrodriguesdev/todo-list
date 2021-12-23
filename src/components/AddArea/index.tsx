723fb765ac89275338424a1b179e2e11da356a22
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-23 15:55:00 -0300

    create style input and div from addArea

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -3,7 +3,12 @@ import * as C from './styles';
 export const AddArea = () => {
   return (
     <C.Container>
-      ...
+      <div className="image">➕</div>
+      <input
+        type="text"
+        name=""
+        placeholder="Adicione uma tarefa"
+      />
     </C.Container>
   );
 }
